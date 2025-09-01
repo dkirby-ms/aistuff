55c61419b59e162e80f4664ae39004d00cffa106
dkirby-ms <saitcho@outlook.com>
2025-09-01 14:51:09 -0500

    refactor

---
+++ inference-scheduling/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export NAMESPACE=llmd # namespace should match existing namespace where HF token secret is stored
+cd ./inference-scheduling
+helmfile apply -e kgateway -n ${NAMESPACE}
+kubectl apply -f httproute.yaml
+
+# Forward the port to Windows host
+minikube service infra-inference-scheduling-inference-gateway --url -n llmd
--- setup_llmd.sh
+++ setup_llmd.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# This script sets up the LLM-D deployer on a local Kubernetes environment with Minikube and Docker, including GPU support. It is intended to be run on a fresh Ubuntu 24.04 install. It has been tested with WSL2 on Win11 with RTX5070.
-git clone https://github.com/llm-d-incubation/llm-d-infra
-
 # Start Minikube with Docker driver and GPU support
 minikube start --driver=docker --container-runtime=docker --gpus=all
 
 # Install dependencies for LLM-D
-bash ./llm-d-infra/quickstart/dependencies/install-deps.sh
+curl -s https://raw.githubusercontent.com/llm-d-incubation/llm-d-infra/refs/heads/main/quickstart/dependencies/install-deps.sh | bash 
 
 # Create kubernetes secret for huggingface token
 # You need to create a token on https://huggingface.co/settings/tokens with read access to private models
-# Then set it as an environment variable HF_TOKEN before running this script
-export HF_TOKEN=""
+# Then EXPORT it as HF_TOKEN in .env.local
+source .env.local
 export HF_TOKEN_NAME=${HF_TOKEN_NAME:-llm-d-hf-token}
-export NAMESPACE=llmd-inference-scheduler # any namespace will do, but must match the one used in the helmfile below
+export NAMESPACE=llmd # any namespace will do, but must match the one used in the helmfile below
 kubectl create namespace ${NAMESPACE}
 kubectl create secret generic ${HF_TOKEN_NAME} \
     --from-literal="HF_TOKEN=${HF_TOKEN}" \
@@ -22,12 +19,6 @@ kubectl create secret generic ${HF_TOKEN_NAME} \
     --dry-run=client -o yaml | kubectl apply -f -
 
 # Install gateway provider CRDs
-bash ./llm-d-infra/quickstart/gateway-control-plane-providers/install-gateway-provider-dependencies.sh
-helmfile apply -f ./llm-d-infra/quickstart/gateway-control-plane-providers/kgateway.helmfile.yaml
+curl -s https://raw.githubusercontent.com/llm-d-incubation/llm-d-infra/refs/heads/main/quickstart/gateway-control-plane-providers/install-gateway-provider-dependencies.sh | bash
+helmfile apply -f https://raw.githubusercontent.com/llm-d-incubation/llm-d-infra/refs/heads/main/quickstart/gateway-control-plane-providers/kgateway.helmfile.yaml
 
-# Install LLM-D for inference scheduling
-sed -i '/^decode:/,/^[a-zA-Z]/ s/  replicas: 2/  replicas: 1/' ./llm-d-infra/quickstart/examples/inference-scheduling/ms-inference-scheduling/values.yaml # hack for only one GPU
-sed -i '/- '\''{"kv_connector":"NixlConnector", "kv_role":"kv_both"}'\''$/a\      - "--gpu-memory-utilization"\n      - "0.85"' ./llm-d-infra/quickstart/examples/inference-scheduling/ms-inference-scheduling/values.yaml
-cd ./llm-d-infra/quickstart/examples/inference-scheduling
-helmfile apply -e kgateway -n ${NAMESPACE}
-kubectl apply -f httproute.yaml
